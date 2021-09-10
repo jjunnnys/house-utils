34a5d911477bc65befb4be20e9470ff156b5340a
jjunnnys <salswnss@gmail.com>
2021-09-10 23:00:41 +0900

    init project

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import type { AppProps /* , AppContext */ } from 'next/app';
+import Sidebar from '@/components/sidebar';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <div style={{ display: 'flex', maxWidth: 1100 }}>
+    <div style={{ flexBasis: '30%', margin: 25 }}>
+      <Sidebar />
+    </div>
+    <div style={{ flexBasis: '70%', margin: 25 }}>
+      <Component {...pageProps} />
+    </div>
+  </div>
+);
+
+export default MyApp;
