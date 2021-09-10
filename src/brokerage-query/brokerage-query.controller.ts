44ddcf84bd9fcc6f029ba2b936a6e4a9257bc885
jjunnnys <salswnss@gmail.com>
2021-09-11 03:01:04 +0900

    add: 주석 달기

--- src/brokerage-query/brokerage-query.controller.ts
+++ src/brokerage-query/brokerage-query.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ActionType } from '@/types/ActionType';
-import { BroKeragePolicyFactory, PurchaseBrokeragePolicy, RentBrokeragePolicy } from './packages';
+import { BroKeragePolicyFactory } from './packages';
 
+/**
+ * 중개수수료가 얼마인지 조회하는 컨트롤러
+ */
 @Controller()
 export class BrokerageQueryController {
-  private policy?: PurchaseBrokeragePolicy | RentBrokeragePolicy;
-
   @Get('api/calc/brokerage')
   calcBrokerage(@Query('actionType') actionType: ActionType, @Query('price') priceString: string): number {
     // type의 처리에 대한 건 factory에서만 분기가 될 수 있게 함
